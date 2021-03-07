b1c45b2bebe48edc595b106a8b4117f480738814
aaaabbc <921864911@qq.com>
2021-03-07 10:24:22 +0800

    v0.0.2

--- src/components/watch.vue
+++ src/components/watch.vue
@@ -3,18 +3,26 @@
     <!-- <p>
         <iframe class="bilivideo" src="//player.bilibili.com/player.html?bvid=BV1nD4y197gW&amp;page=3" frameborder="0" framespacing="0" allowfullscreen="true" > </iframe>
     </p> -->
+    <a :href="url">看看我aaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
   </div>
 </template>
 
 <script>
+import Bar from '../bar'
 export default {
   name: 'watch',
   props: ['IDArr'],
   data () {
     return {
+      url: '',
       msg: 'hello',
       UPname: this.IDArr
     }
+  },
+  created () {
+    Bar.$on('bar', url => {
+      this.url = url
+    })
   }
 }
 </script>
--- src/page/finded/index.vue
+++ src/page/finded/index.vue
@@ -1,22 +1,17 @@
 <template>
     <div class="finded">
-        <button class="backBTN" @click='back'>返回</button>
-        <br/>
-        <input type="text" />
-        <button @click="addItem">添加</button>
-        <ul>
-            <button v-for="(item, index) in getData" :key="item.UID" @click="skip(index)">{{index}}</button>
-        </ul>
-        <p>
+        <bar class="comBar"></bar>
+        <!-- <p>
             <router-link to="/watch">{{ use }}</router-link>
-        </p>
-        <watch :IDArr="IDArr" v-if="false"></watch>
-        <router-view></router-view>
+        </p> -->
+        <watch :IDArr="IDArr" class="comWatch"></watch>
+        <!-- <router-view></router-view> -->
     </div>
 </template>
 
 <script>
 import watch from '../../components/watch'
+import bar from '../../components/bar'
 export default {
   name: 'finded',
   data () {
@@ -27,35 +22,21 @@ export default {
       getData: {}
     }
   },
-  methods: {
-    back () {
-      this.$router.go(-1)
-    },
-    addItem () {
-      // 暂时给页面增加选项，之后要传给后端数据
-    },
-    skip (a) {
-      console.log(this.getData[a]['UID'])
-      // 发给组件watch
-    }
-  },
-  created () {
-    this.$axios.get('../static/value.json').then((resp) => {
-      this.itemLen = Object.keys(resp.data).length
-      for (let i = 0; i < this.itemLen; i++) {
-        this.IDArr.push(Object.keys(resp.data)[i])
-        this.getData = resp.data
-      }
-      // console.log(Object.keys(resp.data)[0])
-    })
-  },
   components: {
-    watch
+    watch,
+    bar
   }
 }
 </script>
 
 <style scoped>
+
+    .bar {
+        width: 20%;
+        height: 100%;
+        min-width: 235px;
+        max-width: 250px;
+    }
 
     .finded {
         background-color: teal;
@@ -65,4 +46,8 @@ export default {
     li {
         list-style: none;
     }
+
+    /* .comWatch {
+        float: right;
+    } */
 </style>
