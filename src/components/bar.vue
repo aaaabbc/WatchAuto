032ebd013ea04f122b4711cd107cce9f8c56aef0
aaaabbc <45326714+aaaabbc@users.noreply.github.com>
2021-03-15 16:11:30 +0800

    Update bar.vue

--- src/components/bar.vue
+++ src/components/bar.vue
@@ -1,6 +1,5 @@
 <template>
-<!-- 这是一个条形导航栏组件，包括添加及选择，通过按钮选择后watch组件会跳转到相应的up主，
-存在一个问题：如何调整子组件在父组件中的位置？ -->
+<!-- 这是一个条形导航栏组件，包括添加及选择，通过按钮选择后watch组件会跳转到相应的up主。 -->
     <div class="bar">
         <div class="finding">
             <input class="addInput" type="text" />
